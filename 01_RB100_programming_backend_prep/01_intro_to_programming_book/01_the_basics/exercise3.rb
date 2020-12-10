06e4181a6e3f86dd71c3859b6953e2416c82c963
Joe Webster <joseph.scott.webster@gmail.com>
2020-12-10 18:01:41 -0500

    Complete exercise 3

---
+++ 01_RB100_programming_backend_prep/01_intro_to_programming_book/01_the_basics/exercise3.rb
@@ -0,0 +1,13 @@
+movies = {
+  jaws: 1975,
+  mean_girls: 2004,
+  iron_man3: 2013,
+  amelie: 2001,
+  indiana_jones: 1981
+}
+
+puts movies[:jaws]
+puts movies[:mean_girls]
+puts movies[:iron_man3]
+puts movies[:amelie]
+puts movies[:indiana_jones]
