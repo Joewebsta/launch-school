344b15d490049cdb10bb322841426984ef65aa73
Joe Webster <joseph.scott.webster@gmail.com>
2020-12-12 10:51:05 -0500

    Complete methods: exercise 5

---
+++ 01_RB100_programming_backend_prep/01_intro_to_programming_book/03_methods/exercise5.rb
@@ -0,0 +1,8 @@
+# ArgumentError: wrong number of arguments (1 for 2)
+# A method was expecting 2 arguments but was only passed 1
+# from (irb):1:in `calculate_product'
+# The method in question is called 'calculate_product' - This is part of the stack trace
+# from (irb):4
+# Perhaps this means line 4 of the irb is where the error occured - This is part of the stack trace
+# from /Users/username/.rvm/rubies/ruby-2.5.3/bin/irb:12:in `<main>'
+# This is a path to the executable file where the error first occurred - This is part of the stack trace
