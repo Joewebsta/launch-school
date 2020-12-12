23fe6ce6d3cc8b48d932f823450dfc381674a5f7
Joe Webster <joseph.scott.webster@gmail.com>
2020-12-12 10:23:52 -0500

    Complete methods: exercise 2

---
+++ 01_RB100_programming_backend_prep/01_intro_to_programming_book/03_methods/exercise2.rb
@@ -0,0 +1,17 @@
+# 1. x = 2
+# evaluates to 2
+
+# 2. puts x = 2
+# writes 2 to the command line
+# returns nil
+
+# 3. p name = "Joe"
+# writes "Joe" to the command line
+# returns nil
+
+# 4. four = "four"
+# evaluates to "four"
+
+# 5. print something = "nothing"
+# writes "nothing" to the command line"
+# returns nil on the same line as the output above (carriage return removed)
