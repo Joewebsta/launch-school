55db2ee574927e421815e26057be4e6f44698be8
Joe Webster <joseph.scott.webster@gmail.com>
2020-12-19 11:35:52 -0500

    Complete strings: exercise 8

---
+++ 01_RB100_programming_backend_prep/02_ruby_basics_exercises/10_strings/08_print_alphabet.rb
@@ -0,0 +1,10 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+puts alphabet.split('')
+
+# i = 0
+# letters = alphabet.split('')
+
+# while i <= (alphabet.length - 1)
+#   puts letters[i]
+#   i += 1
+# end
