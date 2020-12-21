76f241e1a971fdd51252fee7d7ab1f902f0edbf2
Joe Webster <joseph.scott.webster@gmail.com>
2020-12-21 17:11:44 -0500

    Complete hashes exercises

---
+++ 01_RB100_programming_backend_prep/02_ruby_basics_exercises/12_hashes/exercise5.rb
@@ -0,0 +1,9 @@
+numbers = {
+  high: 100,
+  medium: 50,
+  low: 10
+}
+
+numbers.each do |type, value|
+  puts "A #{type} number is #{value}."
+end
---
+++ 01_RB100_programming_backend_prep/02_ruby_basics_exercises/12_hashes/exercise6.rb
@@ -0,0 +1,7 @@
+numbers = {
+  high: 100,
+  medium: 50,
+  low: 10
+}
+
+p half_numbers = numbers.map { |_key, val| val / 2 }
