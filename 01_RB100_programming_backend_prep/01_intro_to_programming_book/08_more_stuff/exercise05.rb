7c05dcd496cff37d086fb6890c6bac35c3c0beb2
Joe Webster <joseph.scott.webster@gmail.com>
2020-12-23 19:17:08 -0500

    Complete more stuff: exercise 5

---
+++ 01_RB100_programming_backend_prep/01_intro_to_programming_book/08_more_stuff/exercise05.rb
@@ -0,0 +1,10 @@
+def execute(&block)
+  block.call
+end
+
+execute { puts 'Hello from inside the execute method!' }
+
+# block.rb1:in `execute': wrong number of arguments (0 for 1) (ArgumentError)
+# from test.rb:5:in `<main>'
+
+# We receive an error because the block parameter needs to be proceeded by an ampersand
