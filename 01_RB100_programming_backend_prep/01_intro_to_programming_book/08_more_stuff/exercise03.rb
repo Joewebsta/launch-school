f79cac5a6f970722b7d48cc9c01f95d19f8203be
Joe Webster <joseph.scott.webster@gmail.com>
2020-12-23 19:11:15 -0500

    Complete more stuff: exercise 3

---
+++ 01_RB100_programming_backend_prep/01_intro_to_programming_book/08_more_stuff/exercise03.rb
@@ -0,0 +1,7 @@
+# Exception handling allows us to anticipate errors in our programs and handle
+# them in such a way that the program does not crash and meaningful information
+# is provided to the programmer so they can troubleshoot the issue.
+
+# Exception handling is a structure used to handle the possibility of an error
+# occurring in a program. It is a way of handling the error by changing the
+# flow of control without exiting the program entirely.
