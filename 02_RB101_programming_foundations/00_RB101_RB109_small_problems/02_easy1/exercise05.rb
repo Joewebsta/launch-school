2ae50ea7c2da1759e9d0680e59294a7818bd012d
Joe Webster <joseph.scott.webster@gmail.com>
2020-12-25 14:49:42 -0500

    Complete easy 1: exercise 5

---
+++ 02_RB101_programming_foundations/00_RB101_RB109_small_problems/02_easy1/exercise05.rb
@@ -0,0 +1,10 @@
+def reverse_sentence(string)
+  words = string.split(' ')
+  reversed = []
+  words.reverse_each { |word| reversed << word }
+  reversed.join(' ')
+end
+
+puts reverse_sentence('') == ''
+puts reverse_sentence('Hello World') == 'World Hello'
+puts reverse_sentence('Reverse these words') == 'words these Reverse'
