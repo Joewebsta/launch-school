3bdcc15a90245046b0afa38ad2b7a8b3caab7a68
Joe Webster <joseph.scott.webster@gmail.com>
2020-12-29 16:45:34 -0500

    Refactor solution 2 to exercise 9

--- 02_RB101_programming_foundations/00_RB101_RB109_small_problems/02_easy1/exercise09.rb
+++ 02_RB101_programming_foundations/00_RB101_RB109_small_problems/02_easy1/exercise09.rb
@@ -1,20 +1,20 @@
 # Brute force solution
 
-def sum(num)
-  sum = 0
+# def sum(num)
+#   sum = 0
 
-  str_digits = num.to_s.chars
+#   str_digits = num.to_s.chars
 
-  str_digits.each do |str_digit|
-    sum += str_digit.to_i
-  end
+#   str_digits.each do |str_digit|
+#     sum += str_digit.to_i
+#   end
 
-  sum
-end
-
-# def sum(number)
-#   number.to_s.chars.reduce(0) { |sum, num| sum + num.to_i }
+#   sum
 # end
+
+def sum(number)
+  number.to_s.chars.map(&:to_i).reduce(:+)
+end
 
 puts sum(23) == 5
 puts sum(496) == 19
