32edccf57edcb4dec248419978188544ae2e29f0
Joe Webster <joseph.scott.webster@gmail.com>
2020-12-29 18:20:21 -0500

    Complete further exploration challenge for Easy 1 Exercise 1

--- 02_RB101_programming_foundations/00_RB101_RB109_small_problems/02_easy1/exercise07.rb
+++ 02_RB101_programming_foundations/00_RB101_RB109_small_problems/02_easy1/exercise07.rb
@@ -1,6 +1,8 @@
-def stringy(size)
+def stringy(size, initial_int = 1)
+  alt_int = (initial_int == 1 ? 0 : 1)
+
   size.times.reduce([]) do |arr, index|
-    arr << (index.even? ? 1 : 0)
+    arr << (index.even? ? initial_int : alt_int)
   end.join
 end
 
@@ -8,3 +10,8 @@ puts stringy(6) == '101010'
 puts stringy(9) == '101010101'
 puts stringy(4) == '1010'
 puts stringy(7) == '1010101'
+
+puts stringy(6, 0) == '010101'
+puts stringy(9, 0) == '010101010'
+puts stringy(4, 0) == '0101'
+puts stringy(7, 0) == '0101010'
