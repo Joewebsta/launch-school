43641d995824204adf8bee7b37e4efd64ddb42c1
Joe Webster <joseph.scott.webster@gmail.com>
2021-01-01 17:50:01 -0500

    Welcome user and prompt for name

--- 02_RB101_programming_foundations/02_small_programs/04_loan_calculator.rb
+++ 02_RB101_programming_foundations/02_small_programs/04_loan_calculator.rb
@@ -1,3 +1,7 @@
+require 'yaml'
+
+MESSAGES = YAML.load_file('04_loan_calculator_messages.yml')
+
 def loan_calculator(amount, apr, duration)
   apr /= 100.0
   monthly_interest_rate = apr / 12
@@ -9,4 +13,17 @@ def calculate_monthly_payment(amount, monthly_interest_rate, duration)
   amount * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**(-duration)))
 end
 
-p loan_calculator(10_000, 6, 128)
+def prompt(input)
+  puts "=> #{input}"
+end
+
+def message(key)
+  MESSAGES[key]
+end
+
+prompt(message('welcome'))
+prompt(message('name_prompt'))
+name = gets.chomp
+prompt("#{message('greeting')} #{name}!")
+
+loan_calculator(10_000, 6, 128)
