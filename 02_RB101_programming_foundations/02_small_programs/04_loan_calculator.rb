4a4bd37eae498b6594979ea4a1fa8e8d463c323d
Joe Webster <joseph.scott.webster@gmail.com>
2021-01-01 19:10:54 -0500

    Add loan amount validation

--- 02_RB101_programming_foundations/02_small_programs/04_loan_calculator.rb
+++ 02_RB101_programming_foundations/02_small_programs/04_loan_calculator.rb
@@ -8,7 +8,8 @@ def loan_calculator(amount, apr, duration)
   apr /= 100
   monthly_interest_rate = apr / 12
 
-  calculate_monthly_payment(amount, monthly_interest_rate, duration).round(2)
+  calculate_monthly_payment(amount.to_f, monthly_interest_rate,
+                            duration).round(2)
 end
 
 def calculate_monthly_payment(amount, monthly_interest_rate, duration)
@@ -27,6 +28,10 @@ def valid_name?(name)
   !name.empty?
 end
 
+def valid_amount?(amount)
+  !(amount.empty? || amount.to_f.negative?)
+end
+
 prompt(message('welcome'))
 prompt(message('name_prompt'))
 
@@ -40,7 +45,12 @@ prompt("#{message('greeting')} #{name}!")
 
 loop do
   prompt(message('amount_prompt'))
-  amount = gets.chomp.to_f
+
+  loop do
+    amount = gets.chomp
+    break if valid_amount?(amount)
+    prompt(message('amount_invalid'))
+  end
 
   prompt(message('apr_prompt'))
   apr = gets.chomp.to_f
