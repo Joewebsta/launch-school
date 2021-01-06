6ad68272f30e397d1c760e45e59b0ee4db8618d9
Joe Webster <joseph.scott.webster@gmail.com>
2021-01-05 19:56:32 -0500

    Complete Easy 2: exercise 6 further exploration exercises

--- 02_RB101_programming_foundations/00_RB101_RB109_small_problems/03_easy2/06_odd_numbers.rb
+++ 02_RB101_programming_foundations/00_RB101_RB109_small_problems/03_easy2/06_odd_numbers.rb
@@ -1,6 +1,12 @@
-num = 1
+# num = 1
 
-while num <= 99
-  puts num
-  num += 2
-end
+# while num <= 99
+#   puts num
+#   num += 2
+# end
+
+# 1.upto(99) do |index|
+#   puts index if index.odd?
+# end
+
+puts Array(1..99).filter(&:odd?)
