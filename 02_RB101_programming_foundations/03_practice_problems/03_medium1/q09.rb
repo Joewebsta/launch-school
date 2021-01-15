56b1fa191741e6e18b7a9844e0830e537818a16b
Joe Webster <joseph.scott.webster@gmail.com>
2021-01-14 20:39:04 -0500

    Complete Lesson 3 Practice Problems - Medium 1 Q9

---
+++ 02_RB101_programming_foundations/03_practice_problems/03_medium1/q09.rb
@@ -0,0 +1,14 @@
+# Consider these two simple methods:
+
+def foo(_param = "no")
+  "yes"
+end
+
+def bar(param = "no")
+  param == "no" ? "yes" : "no"
+end
+
+# What would be the return value of the following method invocation?
+
+p bar(foo)
+# returns "no"
