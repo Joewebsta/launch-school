923450f7003e7b436726d445f5188256043b328b
Joe Webster <joseph.scott.webster@gmail.com>
2021-01-21 15:37:12 -0500

    Complete Easy 5 exercise 1

---
+++ 02_RB101_programming_foundations/00_RB101_RB109_small_problems/06_easy5/01_ascii_string_val.rb
@@ -0,0 +1,12 @@
+# convert string to array
+# convert strings to ascii value
+# sum all elements in the array
+
+def ascii_value(str)
+  str.chars.map(&:ord).sum
+end
+
+p ascii_value('Four score') == 984
+p ascii_value('Launch School') == 1251
+p ascii_value('a') == 97
+p ascii_value('') == 0
