9d3bd456bc957142cb7dd96bb2f459aad510bfb8
Joe Webster <joseph.scott.webster@gmail.com>
2021-01-27 18:46:02 -0500

    Complete easy 7 exercise 2

---
+++ 02_RB101_programming_foundations/00_RB101_RB109_small_problems/08_easy7/02_letter_counter.rb
@@ -0,0 +1,37 @@
+# Separate string into individual characters
+# Iterate over each character
+# Determine if character is lowercase
+# Compare character to char.downcase
+# Determine if character is uppercase
+# Compare character to char.upcase
+# ELSE character is neither
+# When case is determined, increase case count in the hash
+
+UPPERCASE_CHARS = ('A'..'Z').to_a
+LOWERCASE_CHARS = ('a'..'z').to_a
+
+def letter_case_count(string)
+  counts = { lowercase: 0, uppercase: 0, neither: 0 }
+
+  string.chars.each do |char|
+    if UPPERCASE_CHARS.include?(char)
+      counts[:uppercase] += 1
+    elsif LOWERCASE_CHARS.include?(char)
+      counts[:lowercase] += 1
+    else
+      counts[:neither] += 1
+    end
+  end
+
+  counts
+end
+
+p letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
+p letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
+p letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
+p letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
+
+# p letter_case_count('abCdef 123')
+# p letter_case_count('AbCd +Ef')
+# p letter_case_count('123')
+# p letter_case_count('')
