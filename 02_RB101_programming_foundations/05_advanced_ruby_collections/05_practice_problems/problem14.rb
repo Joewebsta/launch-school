338e68ae79492e524e8db09ae4f9efe992f8c8c2
Joe Webster <joseph.scott.webster@gmail.com>
2021-02-06 09:51:55 -0500

    Complete practice problem 14

---
+++ 02_RB101_programming_foundations/05_advanced_ruby_collections/05_practice_problems/problem14.rb
@@ -0,0 +1,23 @@
+hsh = {
+  'grape' => { type: 'fruit', colors: ['red', 'green'], size: 'small' },
+  'carrot' => { type: 'vegetable', colors: ['orange'], size: 'medium' },
+  'apple' => { type: 'fruit', colors: ['red', 'green'], size: 'medium' },
+  'apricot' => { type: 'fruit', colors: ['orange'], size: 'medium' },
+  'marrow' => { type: 'vegetable', colors: ['green'], size: 'large' }
+}
+
+arr = hsh.map do |_name, props|
+  if props[:type] == 'fruit'
+    props[:colors].map(&:capitalize)
+  else
+    props[:size].upcase
+  end
+end
+
+p arr
+
+# [["Red", "Green"], "MEDIUM", ["Red", "Green"], ["Orange"], "LARGE"]
+
+# new array
+# colors of the fruit - Capitalized
+# sizes of the vegtables - Uppercase
