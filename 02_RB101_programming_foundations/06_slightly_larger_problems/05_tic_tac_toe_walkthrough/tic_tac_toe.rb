7a0a096d21d75449ec76598a73810cfaab204211
Joe Webster <joseph.scott.webster@gmail.com>
2021-02-08 18:24:28 -0500

    Setup and display board

---
+++ 02_RB101_programming_foundations/06_slightly_larger_problems/05_tic_tac_toe_walkthrough/tic_tac_toe.rb
@@ -0,0 +1,21 @@
+def initialize_board
+  (1..9).each_with_object({}) { |num, board| board[num] = 'X' }
+end
+
+def display_board(board)
+  puts ""
+  puts "     |     |"
+  puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{board[4]}  |  #{board[5]}  |  #{board[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{board[7]}  |  #{board[8]}  |  #{board[9]}"
+  puts "     |     |"
+end
+
+board = initialize_board
+p display_board(board)
