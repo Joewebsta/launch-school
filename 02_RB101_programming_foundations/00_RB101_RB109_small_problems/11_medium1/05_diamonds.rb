207e8386b5bb350596d5ed3afce1fbb28bc9915e
Joe Webster <joseph.scott.webster@gmail.com>
2021-02-15 16:50:08 -0500

    Refactor diamonds exercise

--- 02_RB101_programming_foundations/00_RB101_RB109_small_problems/11_medium1/05_diamonds.rb
+++ 02_RB101_programming_foundations/00_RB101_RB109_small_problems/11_medium1/05_diamonds.rb
@@ -1,16 +1,29 @@
-def diamond(grid_size)
-  mid_row_num = (grid_size / 2)
+def total_stars(row_num)
+  (2 * row_num) - 1
+end
 
-  (0...mid_row_num).each do |row_num|
-    tot_stars = (2 * row_num) + 1
-    tot_spaces = grid_size - tot_stars / 2
-    puts (' ' * tot_spaces) + ('*' * tot_stars)
+def total_spaces(grid_size, total_stars)
+  grid_size - (total_stars / 2)
+end
+
+def spaces(grid_size, row_num)
+  stars = total_stars(row_num)
+  ' ' * total_spaces(grid_size, stars)
+end
+
+def stars(row_num)
+  '*' * total_stars(row_num)
+end
+
+def diamond(grid_size)
+  mid_row_num = (grid_size / 2.0).ceil
+
+  1.upto(mid_row_num) do |row_num|
+    puts spaces(grid_size, row_num) + stars(row_num)
   end
 
-  (0...(mid_row_num - 1)).reverse_each do |row_num|
-    tot_stars = (2 * row_num) + 1
-    tot_spaces = grid_size - tot_stars / 2
-    puts (' ' * tot_spaces) + ('*' * tot_stars)
+  (mid_row_num - 1).downto(1) do |row_num|
+    puts spaces(grid_size, row_num) + stars(row_num)
   end
 end
 
@@ -26,18 +39,18 @@ end
 # Use 2n-1 formula to determine number of stars
 
 diamond(1)
-# diamond(3)
-# diamond(9)
+diamond(3)
+diamond(9)
 
-#     *
-#    ***
-#   *****
-#  *******
-# *********
-#  *******
-#   *****
-#    ***
-#     *
+# 1     *
+# 2    ***
+# 3   *****
+# 4  *******
+# 5 *********
+# 6  *******
+# 7   *****
+# 8    ***
+# 9     *
 
 # print 1 star
 # print the next odd number of stars (increment previous num by 2)
