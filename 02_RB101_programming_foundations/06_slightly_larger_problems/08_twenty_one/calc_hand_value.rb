3375964a3f5c17606d2684c05d43e78233269809
Joe Webster <joseph.scott.webster@gmail.com>
2021-02-18 17:23:08 -0500

    Further refactoring

--- 02_RB101_programming_foundations/06_slightly_larger_problems/08_twenty_one/calc_hand_value.rb
+++ 02_RB101_programming_foundations/06_slightly_larger_problems/08_twenty_one/calc_hand_value.rb
@@ -1,21 +1,18 @@
+def sum_non_aces(values)
+  values.reduce(0) do |tot, value|
+    tot + (["J", "Q", "K"].include?(value) ? 10 : value.to_i)
+  end
+end
+
 def sum_aces(aces, tot_value)
   total = tot_value
   aces.reduce(total) { |tot| tot + (tot <= 10 ? 11 : 1) }
 end
 
-def sum_non_aces(values, tot_value)
-  total = tot_value
-  values.reduce(total) do |tot, value|
-    tot + (["J", "Q", "K"].include?(value) ? 10 : value.to_i)
-  end
-end
-
 def calc_hand_value(cards)
   values = cards.map { |_suit, val| val }
   aces, values = values.partition { |val| val == "A" }
-
-  tot_value = 0
-  sum_aces(aces, sum_non_aces(values, tot_value))
+  sum_aces(aces, sum_non_aces(values))
 end
 
 p calc_hand_value([["H", "A"], ["C", "A"], ["C", "A"], ["C", "A"]]) == 14
