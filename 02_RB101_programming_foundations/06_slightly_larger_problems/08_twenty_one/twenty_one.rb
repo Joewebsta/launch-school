8c4bdf4a3ed7a7646e143710ea9ca91fcf49a3f1
Joe Webster <joseph.scott.webster@gmail.com>
2021-02-18 17:49:32 -0500

    Display cards dealt to player and their total

--- 02_RB101_programming_foundations/06_slightly_larger_problems/08_twenty_one/twenty_one.rb
+++ 02_RB101_programming_foundations/06_slightly_larger_problems/08_twenty_one/twenty_one.rb
@@ -25,21 +25,60 @@ def deal_cards(deck, participant)
   end
 end
 
-def display_player_cards(player)
-  card_values = player[:cards].map(&:last)
-  card_values = format_card_values(card_values)
-
-  puts "You have: #{card_values}."
-end
-
-def format_card_values(arr)
-  if arr.size == 2
-    arr.join(' and ')
-  else
-    arr[-1] = "and #{arr.last}"
-    arr.join(', ')
+def sum_non_aces(values)
+  values.reduce(0) do |tot, value|
+    tot + (["J", "Q", "K"].include?(value) ? 10 : value.to_i)
   end
 end
+
+def sum_aces(aces, tot_value)
+  total = tot_value
+  aces.reduce(total) { |tot| tot + (tot <= 10 ? 11 : 1) }
+end
+
+def calc_hand_value(cards)
+  # require 'pry'; binding.pry
+  # values = cards.map { |_suit, val| val }
+  aces, values = cards.partition { |val| val == "A" }
+  sum_aces(aces, sum_non_aces(values))
+end
+
+def display_player_cards(player)
+  cards = player[:cards].map(&:last)
+  formatted_cards = format_card_values(cards)
+  total = calc_hand_value(cards)
+
+  prompt "You have: #{formatted_cards}. Total: #{total}."
+end
+
+def format_card_values(cards)
+  if cards.size == 2
+    cards.join(' and ')
+  else
+    cards[-1] = "and #{cards.last}"
+    cards.join(', ')
+  end
+end
+
+def hit(deck, player)
+  player[:cards] << deck.shift
+end
+
+def player_turn(deck, player)
+  action = ''
+  loop do
+    prompt "Would you like to 'hit' or 'stay'?"
+    prompt "Press 'h' for hit or 's' for stay."
+    action = gets.chomp
+    break if action.start_with?('s', 'h')
+    prompt "Sorry, that is an invalid answer."
+  end
+
+  hit(deck, player) if action.start_with?('h')
+end
+
+# MAIN LOGIC
+puts "Welcome to Twenty-One!"
 
 deck = (create_cards('S') + create_cards('D') +
         create_cards('C') + create_cards('H')).shuffle
@@ -47,9 +86,12 @@ deck = (create_cards('S') + create_cards('D') +
 player = { cards: [] }
 dealer = { cards: [] }
 
-# MAIN LOGIC
-puts "Welcome to Twenty-One!"
 deal_cards(deck, player)
 deal_cards(deck, dealer)
-
 display_player_cards(player)
+
+# loop do
+#   player_turn(deck, player)
+#   display_player_cards(player)
+#   break if player[:cards].map { |card| card.last.to_i }.sum > 21
+# end
