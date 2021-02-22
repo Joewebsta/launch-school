844458b02a760a3d5d67d75b1c8b506556f2299a
Joe Webster <joseph.scott.webster@gmail.com>
2021-02-21 19:55:57 -0500

    Complete medium 1 exercise 7

---
+++ 02_RB101_programming_foundations/00_RB101_RB109_small_problems/11_medium1/07_word_to_digit.rb
@@ -0,0 +1,26 @@
+NUMBERS = {
+  'one' => '1',
+  'two' => '2',
+  'three' => '3',
+  'four' => '4',
+  'five' => '5',
+  'six' => '6',
+  'seven' => '7',
+  'eight' => '8',
+  'nine' => '9',
+  'ten' => '10'
+}
+
+def word_to_digit(string)
+  converted_string = string.split.map do |word|
+    if NUMBERS.key?(word) || NUMBERS.key?(word.slice(0..-2))
+      word.end_with?('.') ? "#{NUMBERS[word.slice(0..-2)]}." : NUMBERS[word]
+    else
+      word
+    end
+  end
+
+  converted_string.join(' ')
+end
+
+p word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
