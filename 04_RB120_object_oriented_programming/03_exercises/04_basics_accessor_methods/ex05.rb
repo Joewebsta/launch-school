2dc9276b1ec463be8dbc75f516e2de31444c9b9e
Joe Webster <joseph.scott.webster@gmail.com>
2021-04-17 12:36:37 -0400

    Complete RB120 OO Basics: Accessor Methods exercises

---
+++ 04_RB120_object_oriented_programming/03_exercises/04_basics_accessor_methods/ex05.rb
@@ -0,0 +1,22 @@
+# Add appropriate accessor methods
+# Age should not be visible to intances of Person class
+
+class Person
+  attr_writer :age
+
+  def older_than?(other)
+    age > other.age
+  end
+
+  protected
+
+  attr_reader :age
+end
+
+person1 = Person.new
+person1.age = 17
+
+person2 = Person.new
+person2.age = 26
+
+puts person1.older_than?(person2)
