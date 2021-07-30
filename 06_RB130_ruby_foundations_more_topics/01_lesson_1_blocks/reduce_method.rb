a4d16d0106d9ff0d6b105a081a2db687178448bf
Joe Webster <joseph.scott.webster@gmail.com>
2021-07-30 19:04:35 -0400

    Build reduce method

---
+++ 06_RB130_ruby_foundations_more_topics/01_lesson_1_blocks/reduce_method.rb
@@ -0,0 +1,25 @@
+def reduce(arr, default = 0)
+  index = default.zero? ? 1 : 0
+  acc = default.zero? ? arr[0] : default
+
+  while index < arr.size
+    current_elem = arr[index]
+    acc = yield(acc, current_elem)
+    index += 1
+  end
+
+  acc
+end
+
+# def reduce(arr, result = 0)
+#   index = default.zero? ? 1 : 0
+#   acc = default.zero? ? arr[0] : default
+
+#   arr.each { |elem| acc = yield(acc, elem) }
+#   result
+# end
+
+array = [1, 2, 3, 4, 5]
+p reduce(array) { |acc, num| acc + num }                    # => 15
+p reduce(array, 10) { |acc, num| acc + num }                # => 25
+reduce(array) { |acc, num| acc + num if num.odd? } # => NoMethodError: undefined method `+' for nil:NilClass
