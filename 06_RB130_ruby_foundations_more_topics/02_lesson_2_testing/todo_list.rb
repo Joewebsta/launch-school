d5293d7604bd742918de3c40523e8e45ca72d750
Joe Webster <joseph.scott.webster@gmail.com>
2021-08-13 18:51:53 -0400

    Complete todo list tests and install simpleCov gem

---
+++ 06_RB130_ruby_foundations_more_topics/02_lesson_2_testing/todo_list.rb
@@ -0,0 +1,169 @@
+# This class represents a todo item and its associated
+# data: name and description. There's also a "done"
+# flag to show whether this todo item is done.
+
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+
+  def ==(other_todo)
+    title == other_todo.title &&
+      description == other_todo.description &&
+      done == other_todo.done
+  end
+
+  def title_matches?(other_title)
+    title.downcase.include?(other_title.downcase)
+  end
+end
+
+# This class represents a collection of Todo objects.
+# You can perform typical collection-oriented actions
+# on a TodoList object, including iteration and selection.
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  def add(todo)
+    raise TypeError, 'can only add Todo objects' unless todo.instance_of? Todo
+
+    todos << todo
+  end
+
+  alias << add
+
+  def size
+    todos.size
+  end
+
+  def first
+    todos.first
+  end
+
+  def last
+    todos.last
+  end
+
+  def to_a
+    todos.clone
+  end
+
+  def done?
+    todos.all?(&:done?)
+  end
+
+  def item_at(pos)
+    todos.fetch(pos)
+  end
+
+  def mark_done_at(pos)
+    item_at(pos).done!
+  end
+
+  def mark_undone_at(pos)
+    item_at(pos).undone!
+  end
+
+  def done!
+    todos.each(&:done!)
+  end
+
+  def shift
+    todos.shift
+  end
+
+  def pop
+    todos.pop
+  end
+
+  def remove_at(pos)
+    todos.delete(item_at(pos))
+  end
+
+  def to_s
+    text = "---- #{title} ----\n"
+    text << @todos.map(&:to_s).join("\n")
+    text
+  end
+
+  def each
+    todos.each { |todo| yield(todo) }
+    self
+  end
+
+  def select
+    list = TodoList.new(title)
+
+    each do |todo|
+      list << todo if yield(todo)
+    end
+
+    list
+  end
+
+  def find_by_title(title)
+    select do |todo|
+      return todo if todo.title_matches?(title)
+    end
+  end
+
+  def all_done
+    select(&:done?)
+  end
+
+  def all_not_done
+    select { |todo| !todo.done? }
+  end
+
+  def mark_done(title)
+    each do |todo|
+      if todo.title_matches?(title) && !todo.done?
+        todo.done!
+        return todo
+      end
+    end
+
+    nil
+  end
+
+  def mark_all_done
+    each(&:done!)
+  end
+
+  def mark_all_undone
+    each(&:undone!)
+  end
+
+  private
+
+  attr_reader :todos
+end
