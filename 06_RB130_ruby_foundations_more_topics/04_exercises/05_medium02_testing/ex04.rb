f9b312b7a1c1d669449e9f12d1cd5cc849d67293
Joe Webster <joseph.scott.webster@gmail.com>
2021-08-16 17:53:14 -0400

    Complete RB130 medium exercises 4-6

---
+++ 06_RB130_ruby_foundations_more_topics/04_exercises/05_medium02_testing/ex04.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
+require_relative 'cash_register'
+require_relative 'transaction'
+
+class CashRegisterTest < Minitest::Test
+  def setup
+    @register = CashRegister.new(1000)
+    @transaction = Transaction.new(50)
+  end
+
+  def test_accept_money
+    @transaction.amount_paid = 50
+    prev_amount = @register.total_money
+    @register.accept_money(@transaction)
+
+    assert_equal(prev_amount + 50, @register.total_money)
+  end
+
+  def test_change
+    @transaction.amount_paid = 60
+    assert_equal(10, @register.change(@transaction))
+  end
+end
---
+++ 06_RB130_ruby_foundations_more_topics/04_exercises/05_medium02_testing/ex05.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
+require_relative 'cash_register'
+require_relative 'transaction'
+
+class CashRegisterTest < Minitest::Test
+  def setup
+    @register = CashRegister.new(1000)
+    @transaction = Transaction.new(50)
+  end
+
+  def test_accept_money
+    @transaction.amount_paid = 50
+    prev_amount = @register.total_money
+    @register.accept_money(@transaction)
+
+    assert_equal(prev_amount + 50, @register.total_money)
+  end
+
+  def test_change
+    @transaction.amount_paid = 60
+    assert_equal(10, @register.change(@transaction))
+  end
+
+  def test_give_receipt
+    assert_output("You've paid $50.\n") do
+      @register.give_receipt(@transaction)
+    end
+  end
+end
