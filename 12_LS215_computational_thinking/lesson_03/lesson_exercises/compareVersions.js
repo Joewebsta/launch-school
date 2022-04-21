db5b7a14a45a6a8e5b2de2823c18d8ce486f3180
Joe Webster <joseph.scott.webster@gmail.com>
2022-04-21 10:43:36 -0400

    Complete Compare Versions exercise

--- 12_LS215_computational_thinking/lesson_03/lesson_exercises/compareVersions.js
+++ 12_LS215_computational_thinking/lesson_03/lesson_exercises/compareVersions.js
@@ -1,146 +1,113 @@
 'use strict'
 
-
-/*
-0.1 < 1 = 1.0 < 1.1 < 1.2 = 1.2.0.0 < 1.18.2 < 13.37
-
-1.0 => 1
-1.2.0.0 => 1.2
-
-0.1 < 1 < 1.1 < 1.2 < 1.18.2 < 13.37
-.1 < 1 < 11 < 12 < 
-
-.9534
-
-.1 .9543  1  11  12  118  1337
-
-formatVersion
-remove one or more '.0'
-
-If version contains character other than 0-9 or . - return null
-Split each version into array of individual numbers [1], [1,1], [1,2], [1,18,2]
-
-Count min number of periods
-Match numbers to either side of period count
-compare two numbers
-
-
-Remove the periods
-
-0.1 < 1 = 1.0 < 1.1.3 < 1.1.4 < 1.2 = 1.2.0.0 < 1.18.2 < 13.37
-0.1 < 1 < 1.1.3 < 1.1.4 < 1.2 < 1.18.2 < 13.37
-0.1 < 1 < 1.1.3 < 1.1.4 < 1.2 < 1.18.2 < 13.37
-['0.1'] < ['1'] < ['1', '1', '3'] < ['1', '1', '4'] < ['1', '2'] < [`1`, `18`, `2`] < ['13', '37']
-
-Return null if either version contains characters other than digits and period
-
-Remove any trailing zeros
-If version is less than zero -> multiply by 100
-If version is less than one
-  - Do not alter version
-If version is greater than 1
-  - Split by period '.'
-If version1.length === version2.length
-  - join strings, convert to number and compare versions
-If version1.length !== version2.length
-  - Reduce larger array to length of smaller array
-  join strings, convert to numbers and compare versions
-
-
-Split string by period
-
-10  < 100 < 
-
-([^\d\.]|\.{2,}|^\.|\.$)
-
-\.$|^\.|\.{2,}
-
-[^\.\d]
-*/
-
 function compare(version1, version2) {
   if (invalidVersion(version1) || invalidVersion(version2)) return null;
 
-//  let [ ver1, ver2 ] = [format(version1), format(version2)];
- let ver1 = format(version1); 
- let ver2 = format(version2);
+  let ver1Digits = convertToDigits(version1);
+  let ver2Digits = convertToDigits(version2);
 
- 
- if (ver1.length !== ver2.length) {
-   let smallLength = (ver1.length < ver2.length) ? ver1.length : ver2.length;
-   ver1.length = smallLength;
-   ver2.length = smallLength;
+  let numDigits = Math.max(ver1Digits.length, ver2Digits.length);
+
+  for (let i = 0; i < numDigits; i += 1) {
+    let ver1Digit = ver1Digits[i] || 0;
+    let ver2Digit = ver2Digits[i] || 0;
+
+    if (ver1Digit > ver2Digit) return 1;
+    if (ver1Digit < ver2Digit) return -1;
   }
-  
-  console.log(ver2);
-  
-  ver1 = Number(ver1.join(''))
-  ver2 = Number(ver2.join(''))
 
-  if (ver1 > ver2) return 1;
-  if (ver1 === ver2) return 0;
-  if (ver1 < ver2) return -1;
+  return 0;
 }
 
-function format(version) {
-  if (Number(version) < 1) return [version]; 
-
-  return removeExtraZeros(version).split('.');
-}
-
-function removeExtraZeros(version) {
-  return version.replace(/(\.0)+$/, '')
+function convertToDigits(version) {
+  return version.split('.').map(digit => Number(digit));
 }
 
 function invalidVersion(version) {
   return /[^\d\.]|\.$|^\.|\.{2,}/.test(version);
 }
 
-// console.log(removeExtraZeros('1.2.0.0'));
-// console.log(removeExtraZeros('1.0'));
-
-// function compare(version1, version2) {
-//   if (!(/\./.test(version1))) {
-//     version1 = version1 + '.0'; 
-//   }
-  
-//   if (!(/\./.test(version2))) {
-//     version2 = version2 + '.0'; 
-//   }
-
-//   let v1PeriodCount = version1.match(/\./g).length;
-//   let v2PeriodCount = version2.match(/\./g).length;
-//   let minPeriodCount = Math.min(v1PeriodCount, v2PeriodCount)
-  
-//   let versionRegex = new RegExp(`(\\d+\.){${minPeriodCount}}\\d+`)
-  
-//   let v1Formatted = Number(version1.match(versionRegex)[0].toString().replace('.', ''))
-//   let v2Formatted = Number(version2.match(versionRegex)[0].toString().replace('.', ''))
-  
-//   if (v1Formatted > v2Formatted) return 1;
-//   if (v1Formatted === v2Formatted) return 0;
-//   if (v1Formatted < v2Formatted) return -1;
-// }
-
-// 0.1 < 1 = 1.0 < 1.1 < 1.2 = 1.2.0.0 < 1.18.2 < 13.37
-
+console.log(compare('1', '1'));            // 0
+console.log(compare('1.1', '1.0'));        // 1
+console.log(compare('2.3.4', '2.3.5'));    // -1
+console.log(compare('1.a', '1'));          // null
+console.log(compare('.1', '1'));           // null
+console.log(compare('1.', '2'));           // null
+console.log(compare('1..0', '2.0'));       // null
+console.log(compare('1.0', '1.0.0'));      // 0
+console.log(compare('1.0.0', '1.1'));      // -1
+console.log(compare('1.0', '1.0.5'));      // -1
 
 // console.log(compare('.1.0', '1.0'));
 // console.log(compare('1.0.', '1.0'));
 // console.log(compare('0...1', '1.0'));
 // console.log(compare('!1.0', '1.0'));
+
 // console.log(compare('1.0', '1.0'));
 // console.log(compare('0.954', '1.0'));
 // console.log(compare('1', '1.0'));
-console.log(compare('1.0', '1.1'));
+// console.log(compare('1.0', '1.1'));
+// console.log(compare('1.0.0.0', '1'));
 // console.log(compare('1.1', '1.2'));
 // console.log(compare('1.1', '1.2.0.0'));
+// console.log(compare('1.2.0.0', '1.1'));
 // console.log(compare('1.2.0.0', '1.18.2'));
+// console.log(compare('1.18.2', '1.2.0.0'));
 // console.log(compare('1.18.2', '13.37'));
+// console.log(compare('2.3.4', '2.3.5'));
 
-// function formatVersion(version) {
-  //   return version.toString().replace(/(.0)+/, '');
-  // }
 
-// console.log(formatVersion('1.0'));
-// console.log(formatVersion('1.2.0.0'));
+
+/*
+0.1 < 1 = 1.0 < 1.1.3 < 1.1.4 < 1.2 = 1.2.0.0 < 1.18.2 < 13.37
+
+- Compare the first digit in each version
+- 
+['0', '1']
+['1']
+
+
+[1, 0 , 0]
+[1] 
+
+If accessing an index returns undefined then set the value equal to 0
+
+Will compare 0 to undefined
+If one of the two numbers is undefined
+Then the two numbers are equal - Return 0
+
+V1 [1, 1, 4]
+V2 [1, 1, 3] Return 1
+
+V1 [1, 1, 3]
+V2 [1, 1, 4] Return -1
+
+V1 [1, 2, 0, 0]
+V2 [1, 1, 4] 
+
+V1 [13, 37]
+V2 [1,  1, 4] 
+
+
+Split each string by period
+set ver1Digits to array of digits
+set ver2Digits to array of digits
+
+Determine the longest of the two arrays
+  set numDigits = ver1.length > ver2.length ? ver1.length : ver2.length
+
+- FOR LOOP
+  - Initialize i to 0
+  - Loop until i < numDigits
+  - Increase i by 1
+
+On each iteration
+- set ver1Digit to Number(ver1Digits[i]) || 0
+- set ver2Digit to Number(ver2Digits[i]) || 0
+
+- if ver1Digit > ver2Digit return 1
+- if ver1Digit < ver2Digit return -1
+
+If loop completes iteration without returning a value then return 0
+
+*/
