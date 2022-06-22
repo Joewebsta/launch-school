d81245ed781380b97d837dc19596f8f47828f827
Joe Webster <joseph.scott.webster@gmail.com>
2022-06-22 16:50:08 -0400

    Complete Factory function practice problems 4-5

--- 13_JS225_OOP_JS/05_object_creation_patterns/03_factory_function_practice.js
+++ 13_JS225_OOP_JS/05_object_creation_patterns/03_factory_function_practice.js
@@ -77,3 +77,113 @@ type of object.
 
 // console.log(invoiceTotal(invoices));             // => 31000
 
+// // Problem 4
+
+// function createPayment(services = {}) {
+//   return {
+//     internet: services.internet || 0,
+//     phone: services.phone || 0,
+//     amount: services.amount,
+    
+//     total() {
+//       return this.amount || (this.phone + this.internet);
+//     }
+//   }
+// }
+
+// function paymentTotal(payments) {
+//   let total = 0;
+//   let i;
+
+//   for (i = 0; i < payments.length; i += 1) {
+//     total += payments[i].total();
+//   }
+
+//   return total;
+// }
+
+// let payments = [];
+// payments.push(createPayment());
+// payments.push(createPayment({
+//   internet: 6500,
+// }));
+
+// payments.push(createPayment({
+//   phone: 2000,
+// }));
+
+// payments.push(createPayment({
+//   phone: 1000,
+//   internet: 4500,
+// }));
+
+// payments.push(createPayment({
+//   amount: 10000,
+// }));
+
+// console.log(paymentTotal(payments));      // => 24000
+
+
+// Problem 5
+
+function createInvoice(services = {}) {
+  const payments = [];
+  
+  return {
+    phone: services.phone || 3000,
+    internet: services.internet || 5500,
+    
+    total() {
+      return this.phone + this.internet;
+    },
+
+    addPayment(payment) {
+      payments.push(payment)
+    },
+
+    addPayments(payments) {
+      payments.forEach(payment => this.addPayment(payment));
+    },
+
+    amountDue() {
+      let due = this.total();
+      payments.forEach(payment => due -= payment.total());
+
+      return due;
+    }
+  }
+}
+
+function createPayment(services = {}) {
+  return {
+    internet: services.internet || 0,
+    phone: services.phone || 0,
+    amount: services.amount,
+    
+    total() {
+      return this.amount || (this.phone + this.internet);
+    }
+  }
+}
+
+let invoice = createInvoice({
+  phone: 1200,
+  internet: 4000,
+});
+
+let payment1 = createPayment({
+  amount: 2000,
+});
+
+let payment2 = createPayment({
+  phone: 1000,
+  internet: 1200,
+});
+
+let payment3 = createPayment({
+  phone: 1000,
+});
+
+invoice.addPayment(payment1);
+invoice.addPayments([payment2, payment3]);
+console.log(invoice.amountDue());       // this should return 0
