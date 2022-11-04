9e1d5a2f4d763abcf246de1e39e2f071fe5af533
Joe Webster <joseph.scott.webster@gmail.com>
2022-11-04 16:19:33 -0400

    Complete Guess a Word part 1

--- 14_DOM_Async_Programming/05-Putting_It_All_Together/guess-a-word/javascripts/game.js
+++ 14_DOM_Async_Programming/05-Putting_It_All_Together/guess-a-word/javascripts/game.js
@@ -1,15 +1,50 @@
-let randomWord = (() => {
-  var words = ['apple', 'banana', 'orange', 'pear'];
+document.addEventListener('DOMContentLoaded', () => {
+  const message = document.querySelector('#message');
+  const letters = document.querySelector('#spaces');
+  const guesses = document.querySelector('#guesses');
+  const apples = document.querySelector('#apples');
+  const replay = document.querySelector('#replay');
 
-  return function() {
-    var word = words[Math.floor(Math.random() * words.length)];
-    words.splice(words.indexOf(word), 1);
-    return word;
+
+  let randomWord = (() => {
+    var words = ['apple', 'banana', 'orange', 'pear'];
+  
+    return function() {
+      var word = words[Math.floor(Math.random() * words.length)];
+      words.splice(words.indexOf(word), 1);
+      return word;
+    }
+  })();
+
+  function Game() {
+    this.incorrect = 0;
+    this.lettersGuessed = [];
+    this.correctSpaces = 0;
+    this.word = randomWord();
+    if (!this.word) {
+      this.displayMessage("Sorry, I've run out of words!");
+      return this;
+    }
+    this.word = this.word.split("");
+    this.init();
   }
-})();
+  
+  Game.prototype.createBlanks = function() {
+    let spaces = (new Array(this.word.length + 1)).join("<span></span>");
+    let spans = letters.querySelectorAll("span");
 
-console.log(randomWord());
-console.log(randomWord());
-console.log(randomWord());
-console.log(randomWord());
-console.log(randomWord());
+    spans.forEach(span => span.remove());
+    letters.insertAdjacentHTML("beforeend", spaces);
+    this.spaces = document.querySelectorAll("#spaces span");
+  }
+
+  Game.prototype.init = function() {
+    this.createBlanks();
+  }
+
+  Game.prototype.displayMessage = function(text) {
+    message.innerText = text;
+  }
+  
+  new Game();
+});
