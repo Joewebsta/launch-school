85d864335500f33aceb15024bd3e010c5c602a2d
Joe Webster <joseph.scott.webster@gmail.com>
2022-11-04 11:51:59 -0400

    Refactor deleteItem method

--- 14_DOM_Async_Programming/05-Putting_It_All_Together/inventory_order_form_start_code/javascripts/inventory.js
+++ 14_DOM_Async_Programming/05-Putting_It_All_Together/inventory_order_form_start_code/javascripts/inventory.js
@@ -88,9 +88,10 @@ document.addEventListener('DOMContentLoaded', () => {
       
       deleteItem: function(e) {
         e.preventDefault();
-        var $item = this.findParent(e).remove();
-  
-        this.remove(this.findID($item));
+
+        var item = this.findParent(e);
+        this.remove(this.findID(item));
+        item.remove();
       },
       
       updateItem: function(e) {
